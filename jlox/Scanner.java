b3e2bae2182ef3772129bc6c78273cc07be57655
Duck McSouls <66396308+quacksouls@users.noreply.github.com>
2022-08-03 12:49:52 +1000

    Scan a token.

--- jlox/Scanner.java
+++ jlox/Scanner.java
@@ -8,7 +8,29 @@ import java.util.Map;
 import static com.craftinginterpreters.lox.TokenType.*;
 
 /**
- * Scan the source file and output a bunch of tokens.
+ * Scan the source file and output a bunch of tokens.  A lexeme consists of a
+ * sequence of characters from the source code.  The whole sequence matches the
+ * pattern for a token.  A token in turn is a string that thas a defined
+ * meaning.  A token is structured as a name/value pair.  The token name
+ * specifies the type of a token, the category to which a token belongs.  The
+ * token value is an instance or example of a token.  Here are some common
+ * token names together with sample token values.
+ *
+ * (1) Identifier.  A name chosen by the programmer.
+ *     Examples: myName, last_name, hello.
+ * (2) Keyword.  A reserved name in the programming language.
+ *     Examples: for, if, return.
+ * (3) Separator.  One or more characters that serve as punctuation characters
+ *     or paired-delimiters.
+ *     Examples: {, ;, (
+ * (4) Operator.  A symbol that operates on operands, producing a result.
+ *     Examples: +, >, -
+ * (5) Literal.  A numeric, logical, string, or reference literal.
+ *     Examples: false, 3.1415, "apple"
+ * (6) Comment.  A line or block comment, ignored by the compiler.
+ *     Examples:
+ *     // A line comment.
+ *     This class documentation is a block comment.
  */
 class Scanner {
     /**
@@ -55,6 +77,45 @@ class Scanner {
     }
 
     /**
+     * Scan a token.
+     */
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+        case '(':
+            addToken(LEFT_PAREN);
+            break;
+        case ')':
+            addToken(RIGHT_PAREN);
+            break;
+        case '{':
+            addToken(LEFT_BRACE);
+            break;
+        case '}':
+            addToken(RIGHT_BRACE);
+            break;
+        case ',':
+            addToken(COMMA);
+            break;
+        case '.':
+            addToken(DOT);
+            break;
+        case '-':
+            addToken(MINUS);
+            break;
+        case '+':
+            addToken(PLUS);
+            break;
+        case ';':
+            addToken(SEMICOLON);
+            break;
+        case '*':
+            addToken(STAR);
+            break;
+        }
+    }
+
+    /**
      * Whether we have reached the end of the source code, i.e. we have
      * scanned all characters.
      *
@@ -62,5 +123,35 @@ class Scanner {
      */
     private boolean isAtEnd() {
         return current >= source.length();
+    }
+
+    /**
+     * Advance to the next character in the source code.
+     *
+     * @return The next character in the source code.
+     */
+    private char advance() {
+        current++;
+        return source.charAt(current - 1);
+    }
+
+    /**
+     * Add a token to the list of tokens.
+     *
+     * @param type A token type.
+     */
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    /**
+     * Add a token to the list of tokens.
+     *
+     * @param type A token type.
+     * @param literal A literal value.
+     */
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        token.add(new Token(type, text, literal, line));
     }
 }
